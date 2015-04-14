80c40cd4f763b2e3b23f69e9fbef1bd18e1386ba
liamscofield <liamscofield@gmail.com>
2015-04-14 20:23:05 +0800

    Initial Commit

---
+++ socketDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  socketDemo
+//
+//  Created by AlienLi on 15/4/14.
+//  Copyright (c) 2015年 AlienLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ socketDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  socketDemo
+//
+//  Created by AlienLi on 15/4/14.
+//  Copyright (c) 2015年 AlienLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
